de3ea7b2f220a3112c735a24b04bc39b84881047
Darren Fuller <darren@elastacloud.com>
2022-09-22 08:52:49 +0100

    expand tests to complete coverage of ExportOptions

--- AdxUtils.Options.Tests/ExportOptionsTests.cs
+++ AdxUtils.Options.Tests/ExportOptionsTests.cs
@@ -58,7 +58,8 @@ public class ExportOptionsTests
         parsedPairs.Should().BeEquivalentTo(expected);
     }
 
-    [Fact] public void WhenOptionsIncludeEmptyStrings_WhenPropertyIsCalled_ThenTheInvalidDataIsIgnored()
+    [Fact]
+    public void WhenOptionsIncludeEmptyStrings_WhenPropertyIsCalled_ThenTheInvalidDataIsIgnored()
     {
         var expected = new List<(string, string)>
         {
@@ -74,5 +75,119 @@ public class ExportOptionsTests
         var parsedPairs = options.RenamedTablePairs;
 
         parsedPairs.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void WhenOptionsIncludeNullRename_WhenPropertyIsCalled_ThenAnEmptyArrayisReturned()
+    {
+        var options = new ExportOptions
+        {
+            RenamedTables = null
+        };
+
+        var parsedPairs = options.RenamedTablePairs;
+
+        parsedPairs.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void WhenOptionsIncludeEmptyRename_WhenPropertyIsCalled_ThenAnEmptyArrayisReturned()
+    {
+        var options = new ExportOptions
+        {
+            RenamedTables = Array.Empty<string>()
+        };
+
+        var parsedPairs = options.RenamedTablePairs;
+
+        parsedPairs.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData(new[] {"table1"}, new[] { "table1" })]
+    [InlineData(null, new string[0])]
+    [InlineData(new string[0], new string[0])]
+    public void WhenOptionsIncludeIgnoredTables_WhenAsArrayPropertyIsCalled_ThenDataIsParsedCorrectly(string[] input, string[] expected)
+    {
+        var options = new ExportOptions
+        {
+            IgnoredTables = input
+        };
+
+        var parsedIgnored = options.IgnoredTablesArray;
+
+        parsedIgnored.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [InlineData(new[] {"table1"}, new[] { "table1" })]
+    [InlineData(null, new string[0])]
+    [InlineData(new string[0], new string[0])]
+    public void WhenOptionsIncludeExportDataTables_WhenAsArrayPropertyIsCalled_ThenDataIsParsedCorrectly(string[] input, string[] expected)
+    {
+        var options = new ExportOptions
+        {
+            ExportedDataTables = input
+        };
+
+        var parsedIgnored = options.ExportedDataTablesArray;
+
+        parsedIgnored.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void WhenOptionsIncludeValidSettings_WhenValidated_ThenSuccessful()
+    {
+        var options = new ExportOptions
+        {
+            UseAzureCli = true,
+            Endpoint = "https://a.valid.url/"
+        };
+
+        var act = () => options.Validate();
+
+        act.Should().NotThrow<ArgumentValidationException>();
+    }
+
+    [Fact]
+    public void WhenOptionsIncludeInvalidUrl_WhenValidated_AnExceptionIsThrown()
+    {
+        var options = new ExportOptions
+        {
+            UseAzureCli = true,
+            Endpoint = "/my-endpoint"
+        };
+
+        var act = () => options.Validate();
+
+        act.Should().Throw<ArgumentValidationException>()
+            .WithMessage("*cluster should be a valid, absolute, uri*");
+    }
+
+    [Theory]
+    [InlineData("client_id", "secret", "")]
+    [InlineData("client_id", "secret", "   ")]
+    [InlineData("client_id", "secret", null)]
+    [InlineData("client_id", "", "authority")]
+    [InlineData("client_id", "   ", "authority")]
+    [InlineData("client_id", null, "authority")]
+    [InlineData("", "secret", "authority")]
+    [InlineData("    ", "secret", "authority")]
+    [InlineData(null, "secret", "authority")]
+    public void WhenOptionsIncludeInvalidCredentials_WhenValidated_AnExceptionIsThrown(string clientId,
+        string clientSecret, string authority)
+    {
+        var options = new ExportOptions
+        {
+            ClientId = clientId,
+            ClientSecret = clientSecret,
+            Authority = authority,
+            Endpoint = "https://valid.url"
+        };
+
+        var act = () => options.Validate();
+
+        act.Should().Throw<ArgumentValidationException>()
+            .WithMessage("*id, secret, and authority must be specified*");
     }
 }
