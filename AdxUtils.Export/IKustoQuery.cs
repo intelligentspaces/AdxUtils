95ca5d9ea78890aaf22ba01b5014488563724cd5
ccvaillant1992 <cristinac.vaillant@gmail.com>
2023-08-10 19:35:39 +0200

    Updated util by adding new feature to manage columns in tables

--- AdxUtils.Export/IKustoQuery.cs
+++ AdxUtils.Export/IKustoQuery.cs
@@ -5,4 +5,6 @@ namespace AdxUtils.Export;
 public interface IKustoQuery
 {
     public Task<string> TableDataToCslString(TableSchema table, string tempTableName);
+    Task InsertNewColumnInTable(TableSchema table, string newColumnToInsert, string columnType);
+    Task DropColumnInTable(TableSchema table, string columnToDrop);
 }
